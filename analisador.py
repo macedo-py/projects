dcbdb5332a447551bc431e8df292a315d3d55636
guilherme macedo <macedo.gui0706@gmail.com>
2026-09-09 16:28:58 -0300

    Atualizando lógica do analisador de senhas

--- analisador.py
+++ analisador.py
@@ -1,11 +1,9 @@
 senha = input("Digite sua senha: ")
 
-# Variáveis para registrar o que encontramos na senha
 tem_maiuscula = False
 tem_minuscula = False
 tem_numero = False
 
-# Analisa cada caractere individualmente
 for caractere in senha:
     if caractere.isupper():
         tem_maiuscula = True
@@ -14,7 +12,6 @@ for caractere in senha:
     elif caractere.isdigit():
         tem_numero = True
 
-# Agora basta checar os resultados acumulados
 if tem_maiuscula and tem_minuscula and tem_numero and len(senha) >= 8:
     print("Senha forte!")
 else:
