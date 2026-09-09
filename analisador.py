dc073cc815de1729a548be988afb24c8ddce84fd
guilherme macedo <macedo.gui0706@gmail.com>
2026-09-09 16:53:31 -0300

    Atualização da obrigatoriedade do analisador de senhas

--- analisador.py
+++ analisador.py
@@ -3,6 +3,7 @@ senha = input("Digite sua senha: ")
 tem_maiuscula = False
 tem_minuscula = False
 tem_numero = False
+tem_caractere = False
 
 for caractere in senha:
     if caractere.isupper():
@@ -11,8 +12,10 @@ for caractere in senha:
         tem_minuscula = True
     elif caractere.isdigit():
         tem_numero = True
+    elif not caractere.isalnum():
+        tem_caractere = True
 
-if tem_maiuscula and tem_minuscula and tem_numero and len(senha) >= 8:
-    print("Senha forte!")
+if tem_maiuscula and tem_minuscula and tem_numero and tem_caractere and len(senha) >= 8:
+    print("Senha forte! Aprovada.")
 else:
     print("Senha fraca! Verifique se ela tem no mínimo 8 caracteres, números e letras maiúsculas/minúsculas.")
